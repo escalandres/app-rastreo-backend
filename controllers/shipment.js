e21f3a6e4fea56e640d8a4719507fd4d919ab7b3
Andres Escala <79557629+escalandres@users.noreply.github.com>
2024-12-31 14:36:41 -0600

    modify: new functions added

--- controllers/shipment.js
+++ controllers/shipment.js
@@ -30,12 +30,7 @@ export async function obtenerEmpresasPaqueteria(req, res) {
 }
 
 export async function registrarNuevoEnvio(req, res) {
-    // const { id } = req.params;
     const { containerID, startDate, deliveryDate, shipmentData, locations, shipmentStatus } = req.body;
-
-    // console.log(`containerID: ${containerID}, startDate: ${startDate}, deliveryDate: ${deliveryDate}, shipmentData: ${shipmentData}, locations: ${locations}, shipmentStatus: ${shipmentStatus}`);
-    // console.log("locations", locations);
-    // console.log("shipmentStatus:",shipmentStatus);
 
     const shipment = {
         "container_id": containerID,
@@ -46,13 +41,23 @@ export async function registrarNuevoEnvio(req, res) {
         "shipment_status": shipmentStatus
     };
 
-    console.log("shipment:", shipment);
     const result = await registerNewShipment(shipment);
-    console.log("result:", result);
     if(!result.success){
         return res.status(400).json({success: false, message: result.error});
     }else{
         return res.status(200).json({success: true, message: result.result});
+    }
+}
+
+export async function obtenerContenedoresUsuario(req, res) {
+    console.log("req.query:", req.query);
+    const { userID } = req.query;
+    console.log("containerID:", containerID);
+    const result = await getContainerShipments(containerID);
+    if(!result.success){
+        return res.status(400).json({success: false, message: result.error});
+    }else{
+        return res.status(200).json({success: true, message: result.results});
     }
 }
 
@@ -67,6 +72,7 @@ export async function obtenerEnviosContenedor(req, res) {
         return res.status(200).json({success: true, message: result.results});
     }
 }
+
 export async function obtenerEnvioMasReciente(req, res) {
     console.log("req.query:", req.query);
     const { containerID } = req.query;

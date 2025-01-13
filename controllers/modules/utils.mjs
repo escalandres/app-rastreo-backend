1941e182fa53fe529072be012799eea36c93b38f
Andres Escala <79557629+escalandres@users.noreply.github.com>
2025-01-13 01:59:17 -0600

    modify: console.log deleted

--- controllers/modules/utils.mjs
+++ controllers/modules/utils.mjs
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 export function validateToken(token) { 
     try { 
         const decodedToken = jwt.verify(token, process.env.KEY); 
-        console.log('Token válido'); 
+        // console.log('Token válido'); 
         // Puedes realizar acciones adicionales con el decodedToken aquí 
         return decodedToken; 
     } catch (error) { 

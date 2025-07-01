e9246e65717aa160bcb26095447a21aa3240e9d1
Andres Escala <aescalaa1900@alumno.ipn.mx>
2025-07-01 13:07:51 -0600

    refactor: change on company if

--- controllers/shipment.js
+++ controllers/shipment.js
@@ -87,8 +87,8 @@ export async function processTracker(trackerData) {
             consoleLog('dbResult.result.delivery_date', dbResult.result.delivery_date);
             if(!dbResult.result.delivery_date){
                 let statusInfo = {};
-                if(dbResult.result.shipment_data.company){
-                    switch(dbResult.result.shipment_data.company != ''){
+                if(dbResult.result.shipment_data.company !== ''){
+                    switch(dbResult.result.shipment_data.company){
                         case "DHL":
                             consoleLog('DHL');
                             statusInfo = await DHL(dbResult.result);
